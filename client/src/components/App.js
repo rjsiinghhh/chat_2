a53d9a7f2ddd9a0a7d8e87ee59d6e6ffa3aedcfd
rjsiinghhh <>
2020-12-17 18:15:17 -0800

    3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Login from './Login';
+import useLocalStorage from '../hooks/useLocalStorage';
+import Dashboard from './Dashboard';
 
 
 
 function App() {
-  return (
-    "Hello World"
-  );
+  const [id, setId] = useLocalStorage('id')
+
+
+  return(
+  
+  id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+ )
 }
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function Login() {
+
+export default function Login({ onIdSubmit }) {
+  const idRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+onIdSubmit(idRef.current.value);
+  }
+
+
+  function createNewId() {
+    onIdSubmit(uuidv4())
+  }
+
   return(
-    <div>
-    </div>
+  <Container className="align-items-center d-flex" style= {{ height:'100vh'  }}>
+  <Form onSubmit= {handleSubmit} className="w-100">
+  <Form.Group>
+  <Form.Label>Enter Your ID </Form.Label>
+  <Form.Control type="text" ref={idRef} required></Form.Control>
+  </Form.Group>
+<Button type="submit" className="mr-2"> Login </Button>
+<Button onClick={createNewId} variant="secondary"> Create A New ID</Button>
+  </Form>
+  </Container>
   )
 }
